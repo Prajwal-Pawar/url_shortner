c933d0934cebbf715024520fd145149ac16a1ec8
Prajwal Pawar <prajwalpawar2303@gmail.com>
2022-03-01 16:47:10 +0530

    rendered homepage

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const port = 8000;
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use('/', require('./routes'));
+
 app.listen(port, function(err){
     if(err){
         console.log(`Error starting the server : ${err}`);
